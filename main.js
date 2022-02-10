ea5f822218cef5d5e0618e52373386f2874de2c2
mariosdaskalas <25797700+mariosdaskalas@users.noreply.github.com>
2022-02-10 19:05:11 +0200

    Add Buttons & Slider

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Hi!");
+
+let arr = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let item = arr[Math.floor(Math.random() * arr.length)];
+
+/* RGB Slider */
+
+let rangeRed = document.getElementById("sred").value;
+let rangeGreen = document.getElementById("sgreen").value;
+let rangeBlue = document.getElementById("sblue").value;
+
+function rgbRegenerate() {
+    console.log(`Red: ${rangeRed} / Green : ${rangeGreen} / Blue: ${rangeBlue}`);
+}
