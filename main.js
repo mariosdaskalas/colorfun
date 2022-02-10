f454bfa3d553f26b4eb076753064fab3dfc891d2
mariosdaskalas <25797700+mariosdaskalas@users.noreply.github.com>
2022-02-10 23:26:55 +0200

    Update rgb colors in real time

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ let item = arr[Math.floor(Math.random() * arr.length)];
 
 /* RGB Slider */
 
-let rangeRed = document.getElementById("sred").value;
-let rangeGreen = document.getElementById("sgreen").value;
-let rangeBlue = document.getElementById("sblue").value;
+let sliderRed = document.getElementById("sred");
+sliderRed.addEventListener("input", sliderChange);
+let sliderGreen = document.getElementById("sgreen");
+sliderGreen.addEventListener("input", sliderChange);
+let sliderBlue = document.getElementById("sblue");
+sliderBlue.addEventListener("input", sliderChange);
 
-function rgbRegenerate() {
-    console.log(`Red: ${rangeRed} / Green : ${rangeGreen} / Blue: ${rangeBlue}`);
+function sliderChange() {
+    console.log(`Red: ${sliderRed.value} / Green : ${sliderGreen.value} / Blue: ${sliderBlue.value}`);
+    let rgb = `rgb(${sliderRed.value}, ${sliderGreen.value}, ${sliderBlue.value})`;
+    console.log(`RGB: ${rgb}`);
+    document.getElementById("a").style.backgroundColor = rgb;
+    document.getElementById("f").style.backgroundColor = rgb;
+    document.getElementById("c").style.backgroundColor = rgb;
+    document.getElementById("h").style.backgroundColor = rgb;
 }
