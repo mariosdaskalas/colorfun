e0c65a86d0ae98807a809e4e910ec424cae420cc
mariosdaskalas <25797700+mariosdaskalas@users.noreply.github.com>
2022-02-11 21:13:54 +0200

    Add user interaction

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ function sliderChange() {
     document.getElementById("h").style.backgroundColor = rgb;
 }
 
+/* Reloads the webpage */
+
 function reset() {
     location.reload();
 }
@@ -42,3 +44,36 @@ function generateColor() {
     document.getElementById("g").style.backgroundColor = color;
     document.getElementById("d").style.backgroundColor = color;
 };
+
+/* Checked if element is clicked */
+
+let a = document.getElementById("a");
+let hidden = document.getElementById("hidden");
+let d = document.getElementById("d");
+let f = document.getElementById("f");
+let g = document.getElementById("g");
+let h = document.getElementById("h");
+
+a.addEventListener("click", () => {
+    a.style.display = "none";
+    hidden.style.display = "block";
+});
+
+d.addEventListener("click", () => {
+    d.innerHTML = "Whoohoo, hidden text has been revealed!";
+    d.style.fontSize = "300%";
+    d.style.fontStyle = "italic";
+});
+
+f.addEventListener("click", () => {
+    f.style.opacity = "0.3";
+});
+
+g.addEventListener("click", () => {
+    g.innerHTML = "💖 💖 💖 💖 💖";
+});
+h.addEventListener("click", () => {
+    h.style.backgroundColor = "black";
+});
+
+
